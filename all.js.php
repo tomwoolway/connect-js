b58ea0ae6b3eee9811321feb9dc5014dc619eccc
Tom Woolway <private@tswoolway.co.uk>
2011-04-01 19:11:10 +0100

    Modify build script to only build minimal Facebook library

--- all.js.php
+++ all.js.php
@@ -18,57 +18,57 @@ $JS_FILES = array(
   'src/third-party/json2.js',
 
   'src/core/prelude.js',
-  'src/common/type.js',
+//  'src/common/type.js',
 
   'src/core/api.js',
-  'src/core/auth.js',
+//  'src/core/auth.js',
 
-  'src/core/canvas.js',
+//  'src/core/canvas.js',
   'src/core/content.js',
-  'src/core/cookie.js',
-  'src/core/dialog.js',
-  'src/core/event.js',
+//  'src/core/cookie.js',
+//  'src/core/dialog.js',
+//  'src/core/event.js',
   'src/core/flash.js',
-  'src/core/init.js',
-  'src/core/insights.js',
-  'src/core/intl.js',
+//  'src/core/init.js',
+//  'src/core/insights.js',
+//  'src/core/intl.js',
   'src/core/json.js',
   'src/core/qs.js',
-  'src/core/ui.js',
-  'src/core/ui_methods.js',
-  'src/core/xd.js',
+//  'src/core/ui.js',
+//  'src/core/ui_methods.js',
+//  'src/core/xd.js',
 
-  'src/compat/ui.js',
+//  'src/compat/ui.js',
 
   'src/common/array.js',
-  'src/common/dom.js',
-  'src/common/obj.js',
-  'src/common/string.js',
+//  'src/common/dom.js',
+//  'src/common/obj.js',
+//  'src/common/string.js',
 
-  'src/data/waitable.js',
-  'src/data/query.js',
-  'src/data/data.js',
+//  'src/data/waitable.js',
+//  'src/data/query.js',
+//  'src/data/data.js',
 
-  'src/xfbml/element.js',
-  'src/xfbml/xfbml.js',
-  'src/xfbml/helper.js',
-  'src/xfbml/iframe_widget.js',
-  'src/xfbml/button_element.js',
-  'src/xfbml/edge_widget.js',
+//  'src/xfbml/element.js',
+// 'src/xfbml/xfbml.js',
+// 'src/xfbml/helper.js',
+//  'src/xfbml/iframe_widget.js',
+//  'src/xfbml/button_element.js',
+//  'src/xfbml/edge_widget.js',
 
-  'src/xfbml/tags/activity.js',
-  'src/xfbml/tags/comments.js',
-  'src/xfbml/tags/fan.js',
-  'src/xfbml/tags/like.js',
-  'src/xfbml/tags/live_stream.js',
-  'src/xfbml/tags/loginbutton.js',
-  'src/xfbml/tags/name.js',
-  'src/xfbml/tags/profilepic.js',
-  'src/xfbml/tags/recommendations.js',
-  'src/xfbml/tags/serverfbml.js',
-  'src/xfbml/tags/sharebutton.js',
+//  'src/xfbml/tags/activity.js',
+//  'src/xfbml/tags/comments.js',
+//  'src/xfbml/tags/fan.js',
+//  'src/xfbml/tags/like.js',
+//  'src/xfbml/tags/live_stream.js',
+//  'src/xfbml/tags/loginbutton.js',
+//  'src/xfbml/tags/name.js',
+//  'src/xfbml/tags/profilepic.js',
+//  'src/xfbml/tags/recommendations.js',
+//  'src/xfbml/tags/serverfbml.js',
+//  'src/xfbml/tags/sharebutton.js',
 
-  'src/strings/en_US.js',
+//  'src/strings/en_US.js',
 );
 
 $CSS_FILES = array(
@@ -83,10 +83,10 @@ foreach ($JS_FILES as $file) {
   echo file_get_contents($file);
 }
 
-$css = '';
-foreach ($CSS_FILES as $file) {
-  $css .= file_get_contents($file);
-}
-// css URLs are relative to facebook domains
-$css = preg_replace('#url\(/#', 'url(http://static.ak.fbcdn.net/', $css);
-echo 'FB.Dom.addCssRules(' . json_encode($css) . ', ["pkg"])';
+//$css = '';
+//foreach ($CSS_FILES as $file) {
+//  $css .= file_get_contents($file);
+//}
+//// css URLs are relative to facebook domains
+//$css = preg_replace('#url\(/#', 'url(http://static.ak.fbcdn.net/', $css);
+//echo 'FB.Dom.addCssRules(' . json_encode($css) . ', ["pkg"])';
